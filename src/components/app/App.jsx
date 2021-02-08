7d3e9abdf28092547a46d4c0a351a720e49a62ef
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-02-08 14:25:30 -0800

    init commit

---
+++ src/components/app/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import ListPage from '../../views/ListPage/ListPage.jsx';
+import CharacterDetail from '../../views/CharacterDetail/CharacterDetail.jsx';
+
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/"
+          component={ListPage}
+        />
+        <Route exact path="/details/:id"
+          component={CharacterDetail}
+        />
+      </Switch>
+
+    </Router>
+
+  );
+}
---
+++ src/containers/ListPage.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { fetchCharacters } from '../../services/fetches.js';
+import styles from './ListPage.jsx';
+
+const ListPage = () => {
+    const [characterList, setCharacterList] = useState([]);
+
+    useEffect(async () => {
+        const characterList = await fetchCharacters();
+        setCharacterList(characterList);
+    }, [])
+
+    const characterMap = characterList.map((character, index) => {
+        return <li key={index}>
+            <CharacterPreview {...character} />
+        </li>
+    })
+
+    return (
+        <div>
+            {characterList ?
+                <ul className={styles.listPage}>
+                    {characterMap}
+                </ul>
+                :
+                <p>LOADING!</p>
+            }
+        </div>
+    )
+}
+
+export default ListPage;
